9ecde895fed62521ed210d2c05ce097f0e8649cf
Nathan Ngo <nathanj238@gmail.com>
2024-09-15 01:32:46 +1000

    Added multiplayer functionality

--- characters/warrior/animation_player.gd
+++
@@ -1,7 +0,0 @@
-extends AnimationPlayer
-
-signal animation_attack
-
-
-func attack(hitbox_name: String):
-	animation_attack.emit(hitbox_name)
--- world/world.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-@export var spawn_point: Node2D
-
-
-func get_spawn_point():
-	return spawn_point.position
